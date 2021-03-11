73c658e0d67e06ee5afaf098f519dfc760a9cd3e
hiroto0707 <hiroto130707@gmail.cpm>
2021-03-11 19:25:02 +0900

    修正完了5

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @prototype = Prototype.all
@@ -28,7 +27,7 @@ class PrototypesController < ApplicationController
   def edit
     @prototype = Prototype.find(params[:id])
     unless user_signed_in? && current_user.id == @prototype.user_id
-      redirect_to action: :index
+      redirect_to root_path
     end  
   end
 
@@ -54,9 +53,5 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-     redirect_to action: :index
-    end 
-  end 
+  
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,7 +5,6 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true 
-  validates :image,presence: true
 
   has_many :comments, dependent: :destroy
 
